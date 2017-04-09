13c4626b3446ce1ac4b068b2732b404e0a94e7d7
Marley Plant <marley@marleyplant.com>
2017-04-09 10:42:16 +0100

    css minifying and sourcemaps

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var cleanCSS    = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -17,6 +19,12 @@ gulp.task('serve', ['sass'], function() {
 gulp.task('sass', function() {
     return gulp.src("app/scss/*.scss")
         .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ' Minified: ' + details.stats.minifiedSize);
+        }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
