10d0fa1fdbc26945bc70f0405b167be9b5dfb9a3
Marley Plant <marley@marleyplant.com>
2017-04-19 09:17:01 +0100

    Handle Bower_Components better using main-bower-files

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,24 +1,14 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var cleanCSS    = require('gulp-clean-css');
-var sourcemaps  = require('gulp-sourcemaps');
-var pug         = require('gulp-pug');
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass'),
+    cleanCSS    = require('gulp-clean-css'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    pug         = require('gulp-pug'),
+    filter      = require('gulp-filter'),
+    mainBowerFiles = require('main-bower-files');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'pug'], function() {
 
-    browserSync.init({
-        server: "./dist"
-    });
-
-    gulp.watch("src/scss/**/*.scss", ['sass']);
-    gulp.watch("src/**/*.pug", ['pug']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+gulp.task('sass', function() { // Compile sass into CSS & auto-inject into browsers
     return gulp.src("src/scss/**/*.scss")
         .pipe(sass())
         .pipe(sourcemaps.init())
@@ -28,13 +18,42 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-//Compile pug into HTML & export into dist directory
-gulp.task('pug', function buildHTML() {
+gulp.task('pug', function() { //Compile pug into HTML & export into dist directory
   return gulp.src('src/*.pug')
-  .pipe(pug({
-    pretty: true
-  }))
-  .pipe(gulp.dest("dist/"))
+      .pipe(pug({pretty: true}))
+      .pipe(gulp.dest("dist/"))
 })
 
-gulp.task('default', ['serve']);
+//Handle Bower_Components
+gulp.task('bower-js', function() { //Build and compile bower-js components to the dist directory.
+  return gulp.src(mainBowerFiles())
+      .pipe(filter('**/*.js'))
+      .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('bower-sass', function() { //Build and compile bower-sass components to the dist directory.
+  return gulp.src(mainBowerFiles())
+      .pipe(filter('**/*.scss'))
+      .pipe(sass())
+      .pipe(sourcemaps.init())
+      .pipe(cleanCSS({debug: true}))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest("dist/css"))
+      .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'pug', 'bower'], function() { //Ran as default when command 'gulp' is typed.
+
+    browserSync.init({
+        server: "./dist"
+    });
+
+    gulp.watch("src/scss/**/*.scss", ['sass']);
+    gulp.watch("src/**/*.pug", ['pug']);
+    gulp.watch("dist/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('build', ['sass', 'pug', 'bower']); //Build and compile everything into the dist directory.
+gulp.task('bower', ['bower-js', 'bower-sass']); //Build and compile the Bower_Components.
+gulp.task('default', ['serve']); //Build and Compile everything then run a browserSync server.
